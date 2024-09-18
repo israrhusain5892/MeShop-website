1908b961457b8f67e2594d8450d553571d45ce6c
israrhusain5892 <israrhusain5892@gmail.com>
2024-09-18 17:06:06 +0530

    meshop

--- profile/script.js
+++ profile/script.js
@@ -1,1 +1,17 @@
-// Write your script here
+const hamburger = document.querySelector('.humberger');
+const navLinks = document.querySelector('.nav-items');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+const filterBtn = document.querySelector('.filter-toggle');
+const aside = document.querySelector('.aside-bar');
+filterBtn.addEventListener('click', () => {
+  aside.classList.toggle('filter-btn');
+})
+
+const filterclose = document.querySelector('.filter-close');
+filterclose.addEventListener('click', () => {
+  aside.classList.toggle('filter-btn');
+})
